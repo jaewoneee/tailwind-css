6212a9cf21bfd622361c910bbf9d4ee9c021c031
jaewoneee <jaewon810@naver.com>
2022-03-31 13:27:21 +0900

    feat: create project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+  },
+  plugins: [],
+};
